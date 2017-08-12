4add2d97f606a4f67e3d831bcb19bd5f7b4c10aa
Harry Archer Blackburn <harryblackburn@Harrys-MacBook-Pro.local>
2017-08-12 12:43:18 +0100

    latest updates

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+function check(){
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var question1 = document.question1.value;
+var correct = 0; 
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    if (question1 == "Manchester United FC") {
+        correct++;
+          
+    document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementsById("number_correct").innerHTML = "You got" + correct + "correct.";
 }
